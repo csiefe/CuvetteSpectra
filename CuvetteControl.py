e2a7f2ea13a9159cb33afb8efa6375359fe65502
Claire McLellan <claireallisonmcl@gmail.com>
2019-05-15 11:33:45 -0700

    Simply code to open and read Cuvette holder

--- CuvetteControl.py
+++ CuvetteControl.py
@@ -5,3 +5,18 @@ Created on Wed May 15 10:00:49 2019
 @author: Claire
 """
 
+import serial
+
+COM_PORT = 'com3'
+ser = serial.Serial(COM_PORT)
+
+ser.baudrate = 19200
+ser.timeout = 5
+
+def read():
+    return ser.read_until(b']')
+
+
+def get_id():
+    ser.write(b'[F1 ID ?]')
+    return read()
