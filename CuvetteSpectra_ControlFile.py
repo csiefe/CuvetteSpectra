0b6563037fa5731796451089640531088d549bec
Claire McLellan <claireallisonmcl@gmail.com>
2019-05-30 13:19:25 -0700

    changes to git Control file

--- CuvetteSpectra_ControlFile.py
+++ CuvetteSpectra_ControlFile.py
@@ -16,17 +16,22 @@ import os
 import serial
 import time
 import seabreeze.spectrometers as sb
-from matplotlib import pyplot as plt
-plt.rcParams.update({'font.size': 22})
 import datetime
 import pandas as pd
 import numpy as np
 from Cuvette_Class import Cuvette
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+
+
 qtCreatorFile = "CuvetteSpectra_GUI.ui" # Enter file here.
  
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
  
 class MyApp(QMainWindow):
+    
     def __init__(self):
         super(MyApp, self).__init__()
         self.ui = Ui_MainWindow()
@@ -35,7 +40,8 @@ class MyApp(QMainWindow):
         #initialize the two instruments
         self.ui.init_spec_button.clicked.connect(self.initSpec)
         self.ui.init_cuvette_button.clicked.connect(self.initCuvette)
-
+        self.ui.plot_button.clicked.connect(self.plotSomething)
+        
     def initSpec(self):
         #how do we except 2 different types of error (already connected and not connected at all)
         try:
@@ -57,7 +63,15 @@ class MyApp(QMainWindow):
             COM_NAME = self.ui.comPort.toPlainText()
             self.ui.logOutput.setText('error, either already connected, ComPort was incorrect, or cuvette' +
                                       'holder not connected ' + str(COM_NAME))
+    
+
         
+    def plotSomething(self):
+    
+        self.ui.MplWidget.canvas.axes.clear()
+        self.ui.MplWidget.canvas.axes.plot((np.random.rand(5)))
+        self.ui.MplWidget.canvas.draw()
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
