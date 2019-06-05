cdf18705149a4445cf8df3003849e5fc84e65bf1
csiefe <csiefe@gmail.com>
2019-06-05 11:25:53 -0700

    Can now: plot spectrum and input temperatures

--- CuvetteSpectra_ControlFile.py
+++ CuvetteSpectra_ControlFile.py
@@ -39,16 +39,20 @@ class MyApp(QMainWindow):
         #initialize the two instruments
         self.ui.init_spec_button.clicked.connect(self.initSpec)
         self.ui.init_cuvette_button.clicked.connect(self.initCuvette)
-
+        #Quick spectrum plot
         self.ui.collect_spectrum_button.clicked.connect(self.plotSomething)
+        #Temp Series Experiment
+        self.ui.start_temp_series_button.clicked.connect(self.tempSeries)
         
-
+        
     def initSpec(self):
         #how do we except 2 different types of error (already connected and not connected at all)
         try:
+            global spec
             spec = sb.Spectrometer.from_serial_number()
             x = 'device was initialized'
             self.ui.logOutput.setText(x)
+            return spec
         except:
             x = 'Device is opened'
             self.ui.logOutput.setText(x)
@@ -56,21 +60,59 @@ class MyApp(QMainWindow):
     def initCuvette(self):
         
         try:
+            global cuvette
             COM_NAME = str(self.ui.comPort.text())
             cuvette = Cuvette.open_from_port(COM_NAME)
             text = "Serial port is being opened"
             self.ui.logOutput.setText(text) 
+            return cuvette
         except:
             COM_NAME = self.ui.comPort.toPlainText()
             self.ui.logOutput.setText('error, either already connected, ComPort was incorrect, or cuvette' +
                                       'holder not connected ' + str(COM_NAME))
 
     def plotSomething(self):
-    
+        wavelengths = spec.wavelengths()
+        intensities = spec.intensities()
         self.ui.MplWidget.canvas.axes.clear()
-        self.ui.MplWidget.canvas.axes.plot((np.random.rand(5)))
+        self.ui.MplWidget.canvas.axes.plot(wavelengths, intensities)
+        self.ui.MplWidget.canvas.axes.set_xlabel('Wavelength (nm)')
+        self.ui.MplWidget.canvas.axes.set_ylabel('Intensity (counts)')
         self.ui.MplWidget.canvas.draw()
+        current_temp = cuvette.get_current_temp()
+        self.ui.displayCurrentTemp.display(current_temp)
             
+    
+    def tempSeries(self):
+        start_temp = self.ui.start_temp_temp_series.value()
+        end_temp = self.ui.end_temp_temp_series.value()
+        temp_int = self.ui.temp_int_temp_series.value()
+        Temp = np.arange(start_temp, end_temp, temp_int)
+        cuvette.temp_control_on()
+
+        for t in Temp:
+            cuvette.set_temp(t)
+            current_temp = cuvette.get_current_temp()
+            self.ui.displayCurrentTemp.display(current_temp)
+            
+            while t != temp:
+                time.sleep(5)
+                temp = cuvette.get_current_temp()
+
+
+    #spectra = plt.figure()
+    wavelengths = spec.wavelengths()
+    intensities = spec.intensities()
+    #make this plot more readable 
+    #spectra.set(xlabel = 'Wavelength (nm)', ylabel = 'Intensity (a.u.)', 
+                #title = "Spectra at temperature {} C".format(t)+r'$^\circ$')
+    df2 = pd.DataFrame({"intensities": intensities})
+    df3 = pd.DataFrame({"temp": np.array([t])})
+    df1 = pd.DataFrame({"wavelengths": wavelengths})
+    df = pd.concat([df1,df2,df3], ignore_index = True, axis = 1)
+    df.to_csv(path + "test{}.csv".format(t), index = False, header = ["WL", "Int", "temp"])
+    
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
